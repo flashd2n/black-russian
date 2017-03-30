bfd84378e860f3fc854e06cd3583867dd8daa2b1
flash <k.kostov.eu@gmail.com>
2017-03-30 03:39:11 +0300

    refactor progress improved cohesion, improved naming, reduced coupling

--- app/cards.js
+++ app/cards.js
@@ -1,12 +1,12 @@
-"use strict";
-var stage = new Konva.Stage({
+'use strict';
+const stage = new Konva.Stage({
     container: 'canvas',
     width: 1600,
     height: 2000
 });
 
-var layer = new Konva.Layer();
-var text = new Konva.Text({
+const layer = new Konva.Layer();
+const text = new Konva.Text({
     x: 10,
     y: 10,
     fontFamily: 'Calibri',
@@ -14,17 +14,17 @@ var text = new Konva.Text({
     text: '',
     fill: 'black'
 });
+const cards = [];
+const colors = ['pink', 'yellow', 'green', 'orange', 'blue', 'red'];
+const actualColors = [];
 
-var colors = ["pink", "yellow", "green", "orange", "blue", "red"];
-var actualColors = [];
-
-var startTimer;
 let xOffset = 0;
 
-var colorsWhichAreSelected = [];
+const colorsWhichAreSelected = [];
+
 function getRandomColor() {
-    var availableColors = colors.length - 1;
-    var currentColor = colors[Math.round(Math.random() * availableColors)];
+    let availableColors = colors.length - 1;
+    let currentColor = colors[Math.round(Math.random() * availableColors)];
     if (colorsWhichAreSelected.indexOf(currentColor) < 0) {
         colorsWhichAreSelected.push(currentColor);
     }
@@ -35,50 +35,43 @@ function getRandomColor() {
 
 }
 
-
 //* EASY NORMAL HARD difficulty
-var items = 0;
 
 $(document).ready(function () {
-    var $easy = $('.easy');
-    var $normal = $('.normal');
-    var $hard = $('.hard');
+    const $easy = $('.easy');
+    const $normal = $('.normal');
+    const $hard = $('.hard');
 
-    var clicked = false;
-
-    //* TO DO: change ITEMS values for final version
+    let clicked = false;
 
     $easy.click(function () {
         if (clicked === false) {
-            items = 15;
-            console.log(items);
-            createObjects(items);
+            let items = 16;
+            let cardColors = generateRandomColorsArr(items);
+            initiateField(16, cardColors);
             clicked = true;
         }
     });
     $normal.click(function () {
         if (clicked === false) {
-            items = 42;
-            console.log(items);
-            createObjects(items);
+            let items = 42;
+            let cardColors = generateRandomColorsArr(items);
+            initiateField(42, cardColors);
             clicked = true;
         }
     });
     $hard.click(function () {
         if (clicked === false) {
-            items = 84;
-            console.log(items);
-            createObjects(items);
+            let items = 84;
+            let cardColors = generateRandomColorsArr(items);
+            initiateField(84, cardColors);
             clicked = true;
         }
     });
 });
 
-//* > added var Items to the total amount of shapes in the game*/
-//* > everything was added to a function */
-
-
 function generateRandomColorsArr(count) {
+
     let cardColors = [];
 
     for (let i = 0; i < count / 2; i += 1) {
@@ -87,20 +80,23 @@ function generateRandomColorsArr(count) {
         cardColors.push(colors[rndIndex]);
     }
 
-    // console.log(cardColors);
-
     return cardColors;
 }
 
-function createObjects(items) {
-    let cardColors = generateRandomColorsArr(items);
+
+
+
+function initiateField(items, cardColors) {
+
     let rows = 0;
-    var currentCard;
-
-
     xOffset = 0;
+
+    let currentCard;
+
     for (var i = 0; i < items; i += 1) {
+
         let randomColorIndex = Math.floor(Math.random() * cardColors.length);
+
         let realColor = cardColors[randomColorIndex];
         cardColors.splice(randomColorIndex, 1);
 
@@ -108,169 +104,171 @@ function createObjects(items) {
         if (i % 10 === 0 && i !== 0) {
             ++rows;
         }
+
         currentCard = createCard(realColor, i, rows);
-
-        actualColors.push(realColor);
-
         layer.add(currentCard);
 
+        actualColors.push(realColor);
     }
 
     stage.add(layer);
 
-
-
-
-
     setTimeout(function () {
 
         layer.destroy();
+
         let rows = 0;
         xOffset = 0;
-        for (var i = 0; i < items; i += 1) {
 
+        for (var i = 0; i < items; i += 1) {
 
             if (i % 10 === 0 && i !== 0) {
                 ++rows;
             }
 
             currentCard = createCard('gray', i, rows);
-
             layer.add(currentCard);
-
-            currentCard.tween = new Konva.Tween({
-                node: currentCard,
-                scaleX: 1.2,
-                scaleY: 1.2,
-                x: currentCard.attrs.x - 10,
-                y: currentCard.attrs.y - 10,
-                easing: Konva.Easings.EaseIn,
-                duration: 0.3
-            });
-
+            currentCard.tween = generateAnimation(currentCard);
         }
+
         layer.add(text);
         stage.add(layer);
 
-        startTimer = new Date();
-
-
-        layer.on('mouseover', function (event) {
-
-            event.target.tween.play();
-
-        });
-
-        layer.on('mouseout', function (event) {
-
-            event.target.tween.reverse();
-
-        });
-
-
-        layer.on("click tap", function (evt) {
-            var shapes = stage.find('Rect');
-
-            // if card is flipped face-down
-            if (evt.target.fill() === 'gray') {
-                evt.target.fill(actualColors[evt.target.id()]);
-            }
-
-            writeMessage('Selected card is ' + evt.target.fill() + " with ID " + evt.target.id());
-
-            storeCards(evt);
-            if (isSecond()) {
-
-                if (checkSameType(cards) && !checkIfFlipped(cards)) {
-
-                    if (shapes.length === 2) {
-                        let currentTime = new Date();
-                        let endTimerInSeconds = (currentTime - startTimer) / 1000;
-                        let timeToComplete = Math.round(endTimerInSeconds * 100) / 100;
-
-                        writeMessage('Time: ' + timeToComplete + 'seconds');
-                    }
-
-
-
-                    destroy(cards);
-                }
-                else {
-                    shapes.each(function (shape) {
-                        shape.fill('gray');
-                    });
-                }
-                cards = [];
-            }
-
-            //removeCards(evt.target);
-
-        });
-
+        startGame();
 
     }, 1000);
+}
 
 
+function generateAnimation(card) {
 
-    //write message function
-    function writeMessage(message) {
-        text.setText(message);
-        layer.draw();
-    }
+    let animation = new Konva.Tween({
+        node: card,
+        scaleX: 1.2,
+        scaleY: 1.2,
+        x: card.attrs.x - 10,
+        y: card.attrs.y - 10,
+        easing: Konva.Easings.EaseIn,
+        duration: 0.2
+    });
 
-    function checkIfFlipped(cards) {
-        var firstCardColor = cards[0].target.id();
-        var secondCardColor = cards[1].target.id();
+    return animation;
+}
 
-        if (firstCardColor === secondCardColor) {
-            return true;
+function startGame() {
+
+    let startTimer = new Date();
+
+    layer.on('mouseover', function (event) {
+
+        event.target.tween.play();
+
+    });
+
+    layer.on('mouseout', function (event) {
+
+        event.target.tween.reverse();
+
+    });
+
+
+    layer.on('click tap', function (evt) {
+        const shapes = stage.find('Rect');
+
+        // if card is flipped face-down
+        if (evt.target.fill() === 'gray') {
+            evt.target.fill(actualColors[evt.target.id()]);
         }
-    }
 
-    //switches every even click on a different card
-    var isSecond = (function () {
-        var called = true;
-        return function () {
-            if (called === true) {
-                called = false;
-            } else {
-                called = true;
+        writeMessage('Selected card is ' + evt.target.fill() + ' with ID ' + evt.target.id());
+
+        storeCards(evt);
+        if (isSecond()) {
+
+            if (checkSameType(cards) && !checkIfFlipped(cards)) {
+
+                if (shapes.length === 2) {
+
+                    gameEnd(startTimer);
+
+                }
+
+                destroy(cards);
             }
-            return called;
-        };
-    })();
-
-
-    //function to store two cards
-    var cards = [];
-
-    function storeCards(card) {
-        cards.push(card);
-
-    }
-
-    //function to check if cards are the same type
-    function checkSameType(cards) {
-        var firstCardColor = cards[0].target.fill();
-        var secondCardColor = cards[1].target.fill();
-
-        if (firstCardColor === secondCardColor) {
-            return true;
+            else {
+                shapes.each(function (shape) {
+                    shape.fill('gray');
+                });
+            }
+            cards.splice(0, cards.length);
         }
-    }
+    });
+}
 
-    //destroy cards
+function gameEnd(startTimer) {
+    let currentTime = new Date();
+    let endTimerInSeconds = (currentTime - startTimer) / 1000;
+    let timeToComplete = Math.round(endTimerInSeconds * 100) / 100;
 
-    function destroy(cards) {
-        cards[1].target.destroy();
-        layer.draw();
-        cards[0].target.destroy();
-        layer.draw();
-        return this;
+    writeMessage('Time: ' + timeToComplete + 'seconds');
+}
+
+
+//write message function
+function writeMessage(message) {
+    text.setText(message);
+    layer.draw();
+}
+
+function checkIfFlipped(cards) {
+    var firstCardColor = cards[0].target.id();
+    var secondCardColor = cards[1].target.id();
+
+    if (firstCardColor === secondCardColor) {
+        return true;
     }
+}
+
+//switches every even click on a different card
+var isSecond = (function () {
+    var called = true;
+    return function () {
+        if (called === true) {
+            called = false;
+        } else {
+            called = true;
+        }
+        return called;
+    };
+})();
+
+//function to store two cards
+
+function storeCards(card) {
+    cards.push(card);
 
 }
 
+
+//function to check if cards are the same type
+function checkSameType(cards) {
+    var firstCardColor = cards[0].target.fill();
+    var secondCardColor = cards[1].target.fill();
+
+    if (firstCardColor === secondCardColor) {
+        return true;
+    }
+}
+
+//destroy cards
+
+function destroy(cards) {
+    cards[1].target.destroy();
+    layer.draw();
+    cards[0].target.destroy();
+    layer.draw();
+    return this;
+}
 
 
 function createCard(color, id, rows) {
@@ -292,7 +290,6 @@ function createCard(color, id, rows) {
     });
 
     ++xOffset;
-
 
     return card;
 }
